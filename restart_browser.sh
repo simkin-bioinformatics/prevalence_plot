d8161d0f7fe588a14d038be540604ad9d453a623
Charlie Simkin <charlie.simkin@gmail.com>
2024-11-15 14:54:51 -0500

    change sif_file to use local sif instead of remote bind in the current working directory in case the instance is started outside of the user's home folder

--- restart_browser.sh
+++ restart_browser.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 PORT=1234
-SIF_FILE='library://csimkin/dash/dash_server'
+SIF_FILE='dash_server.sif'
 
 singularity instance stop dash_server
-singularity instance start $SIF_FILE dash_server $PORT
+singularity instance start -B $(pwd -P):$(pwd -P) $SIF_FILE dash_server $PORT
 
 echo "running on port $PORT, if local can access at http://localhost:$PORT"
